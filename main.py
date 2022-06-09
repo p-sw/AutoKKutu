01eda51635b3f8bee17ce61a574ab9a866ad8162
sserve-kr <61446372+sserve-kr@users.noreply.github.com>
2022-06-09 11:28:04 +0000

    Added wheel installation when ImportError

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ for _ in range(3):
         log.warning('Required modules not found.')
         log.info('Installing..')
         import pip
+        pip.main(['install', 'wheel'])
         pip.main(['install', '-r', 'requirements.txt'])
         log.success('Successfully installed all modules.')
         log.info('Retrying imports..')

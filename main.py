3c3eaf6df382de9699ee06d1cc1a63e32d85bb2b
sserve-kr <61446372+sserve-kr@users.noreply.github.com>
2022-06-09 10:28:57 +0000

    Added auto import fix & logs

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
 import json
 import sys
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
-
 from lib.logger import Logger
+log = Logger()
+
+for _ in range(3):
+    try:
+        from selenium import webdriver
+        from selenium.webdriver.common.by import By
+        from selenium.webdriver.support.ui import WebDriverWait
+        from selenium.webdriver.support import expected_conditions as EC
+        from webdriver_manager.chrome import ChromeDriverManager
+        break
+    except ImportError:
+        log.warning('Required modules not found.')
+        log.info('Installing..')
+        import pip
+        pip.main(['install', '-r', 'requirements.txt'])
+        log.success('Successfully installed all modules.')
+        log.info('Retrying imports..')
+log.success('Successfully imported all modules.')
+
 
 initial = "https://kkutu.co.kr/o/login/"
 current_account = None
 
-logger = Logger()
-
 with open("account.config", "r", encoding="utf-8") as f:
     accountConfig = json.load(f)
+    log.success('Successfully loaded account config.')
 
 with open("global.config", "r", encoding="utf-8") as f:
     globalConfig = json.load(f)
+    log.success('Successfully loaded global config.')
 
 for account in accountConfig:
     if not account["use"]:
@@ -27,9 +40,10 @@ for account in accountConfig:
     initial += account["method"]
 
 if not current_account:
-    logger.error("No account is available. If you want to use account, please set use to true.")
+    log.error("No account is available. If you want to use account, please set use to true.")
     sys.exit(1)
 
+log.info(f"Using account:\nLOGIN_METHOD:{current_account['method']}\nACCOUNT_ID:{current_account['account_info']['id']}\nACCOUNT_PW:{current_account['account_info']['pw']}")
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
@@ -37,6 +51,7 @@ driver.get(initial)
 
 match current_account['method']:
     case "facebook":
+        log.info('Using facebook login method.')
         WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.ID, "email"))
         )
@@ -44,6 +59,7 @@ match current_account['method']:
         driver.find_element_by_id("pass").send_keys(current_account['account_info']['password'])
         driver.find_element_by_id("loginbutton").click()
     case "naver":
+        log.info('Using naver login method.')
         WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.ID, "id"))
         )
@@ -51,6 +67,7 @@ match current_account['method']:
         driver.find_element_by_id("pw").send_keys(current_account['account_info']['password'])
         driver.find_element_by_id("log.login").click()
     case "google":
+        log.info('Using google login method.')
         WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.ID, "identifierId"))
         )
@@ -59,6 +76,7 @@ match current_account['method']:
         driver.find_element_by_name("password").send_keys(current_account['account_info']['password'])
         driver.find_element_by_id("passwordNext").click()
     case "kakao":
+        log.info('Using kakao login method.')
         WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.ID, "id_email_2"))
         )
@@ -66,12 +84,14 @@ match current_account['method']:
         driver.find_element_by_id("id_password_3").send_keys(current_account['account_info']['password'])
         driver.find_element_by_xpath('//*[@id="login-form"]/fieldset/div[8]/button[1]').click()
     case "twitter":
+        log.info('Using twitter login method.')
         WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.ID, "username_or_email"))
         )
         driver.find_element_by_id("username_or_email").send_keys(current_account['account_info']['id'])
         driver.find_element_by_id("password").send_keys(current_account['account_info']['password'])
         driver.find_element_by_id("allow").click()
+log.info('Successfully logged in.')
     
 WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.ID, "kkuko"))

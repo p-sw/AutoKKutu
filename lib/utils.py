f8a270dc5d8b697069030b232ee5758bbbe1fa44
sserve-kr <61446372+sserve-kr@users.noreply.github.com>
2022-06-13 12:37:30 +0000

    Added delay min in send_keys_delay

--- lib/utils.py
+++ lib/utils.py
@@ -6,14 +6,14 @@ class DriverWrapper:
     def __init__(self, driver_obj):
         self.driver = driver_obj
 
-    def send_keys_delay(self, element, *values, delay_ms=200, random=True):
+    def send_keys_delay(self, element, *values, delay_max=100, delay_min=50, random=True):
         for value in values:
             for key in value:
                 element.send_keys(key)
                 if random:
-                    time.sleep(randint(0, delay_ms) / 1000)
+                    time.sleep(randint(delay_min, delay_max) / 1000)
                 else:
-                    time.sleep(delay_ms / 1000)
+                    time.sleep(delay_max / 1000)
 
 def return_append(origin, new):
     temp = deepcopy(origin)

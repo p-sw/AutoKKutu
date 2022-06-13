46a9f0303efa8e5d058e531e236ab49e99a18e34
sserve-kr <61446372+sserve-kr@users.noreply.github.com>
2022-06-13 11:34:56 +0000

    Added database manager

---
+++ lib/dbm.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+class By:
+    LOW_LENGTH = 0
+    HIGH_LENGTH = 1
+
+class DBManager:
+    def __init__(self):
+        self.db = sqlite3.connect('word.db')
+    
+    def get_word(self, by, value, dups):
+        match by:
+            case By.LOW_LENGTH:
+                sql = f'SELECT word FROM stdict WHERE word LIKE "{value}%" ORDER BY length ASC'
+            case By.HIGH_LENGTH:
+                sql = f'SELECT word FROM stdict WHERE word LIKE "{value}%" ORDER BY length DESC'
+
+        cursor = self.db.cursor()
+        match_words = cursor.execute(sql).fetchall()
+        for dup_word in dups:
+            try:
+                match_words.remove(dup_word)
+            except ValueError:
+                pass  # ignore if value is not exists in matched words
+        return match_words
